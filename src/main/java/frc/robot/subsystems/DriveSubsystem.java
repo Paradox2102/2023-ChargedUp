8ff3af4ab38219102255f6d863b290d7069f0eea
cpaggron <christopher.periodictable.pi@gmail.com>
2023-01-08 12:50:56 -0800

    Created Location Tracker

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -17,28 +17,6 @@ public class DriveSubsystem extends SubsystemBase {
     // motor stuff
   }
 
-  public double[] getLocation(double x, double z, double theta, double[] target) {
-    double[] location = new double[2];
-        double tx = target[0];
-        double ty = target[1];
-        for (double i = theta; i >= 360; i = theta){
-            theta -= 360;
-        }
-
-        double alpha = Math.atan(x/z);
-        double beta = theta - 90 - alpha;
-        double d = Math.sqrt(x*x + z*z);
-        double dx = d * Math.sin(beta);
-        double dy = d * Math.cos(beta);
-        double robot_x = tx + dx;
-        double robot_y = ty - dy;
-
-        location[0] = robot_x;
-        location[1] = robot_y;
-
-        return location;
-  }
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
