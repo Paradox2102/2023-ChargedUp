1f7036a795d7d6d83e2c6a95137cc9ad99007ea7
cpaggron <christopher.periodictable.pi@gmail.com>
2023-01-08 13:11:03 -0800

    Edited Location Tracking

--- src/main/java/frc/robot/LocationTracker.java
+++ src/main/java/frc/robot/LocationTracker.java
@@ -4,24 +4,24 @@ public class LocationTracker {
 
     public double[] getLocation(double x, double z, double theta, double[] target) {
         double[] location = new double[2];
-            double tx = target[0];
-            double ty = target[1];
-            for (double i = theta; i >= 360; i = theta){
-                theta -= 360;
-            }
-    
-            double alpha = Math.atan(x/z);
-            double beta = theta - 90 - alpha;
-            double d = Math.sqrt(x*x + z*z);
-            double dx = d * Math.sin(beta);
-            double dy = d * Math.cos(beta);
-            double robot_x = tx + dx;
-            double robot_y = ty - dy;
-    
-            location[0] = robot_x;
-            location[1] = robot_y;
-    
-            return location;
+        double tx = target[0];
+        double ty = target[1];
+        for (double i = theta; i >= 360; i = theta){
+            theta -= 360;
+        }
+
+        double alpha = Math.atan(x/z);
+        double beta = theta - 90 - alpha;
+        double d = Math.sqrt(x*x + z*z);
+        double dx = d * Math.sin(beta);
+        double dy = d * Math.cos(beta);
+        double robot_x = tx + dx;
+        double robot_y = ty - dy;
+
+        location[0] = robot_x;
+        location[1] = robot_y;
+
+        return location;
       }
     
 }
