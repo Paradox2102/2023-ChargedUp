3881fd99935c8e22b1596d70dafe39600c444815
cpaggron <christopher.periodictable.pi@gmail.com>
2023-01-09 11:32:02 -0800

    Edited Location Tracker

--- src/main/java/frc/robot/LocationTracker.java
+++ src/main/java/frc/robot/LocationTracker.java
@@ -1,16 +1,16 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class LocationTracker {
 
-    public double[] getLocation(double x, double z, double theta, double[] target) {
-        double toRadian = Math.PI/180;
-        double[] location = new double[2];
+    public Pose2d getLocation(double x, double z, double theta, double[] target) {
         double tx = target[0];
         double ty = target[1];
         for (double i = theta; i >= 360; i = theta){
             theta -= 360;
         }
-        theta *= toRadian;
 
         double alpha = Math.atan(x/z);
         double beta = theta - 90 - alpha;
@@ -20,10 +20,7 @@ public class LocationTracker {
         double robot_x = tx + dx;
         double robot_y = ty - dy;
 
-        location[0] = robot_x;
-        location[1] = robot_y;
-
-        return location;
+        return new Pose2d(robot_x, robot_y, Rotation2d.fromDegrees(theta));
       }
     
 }
