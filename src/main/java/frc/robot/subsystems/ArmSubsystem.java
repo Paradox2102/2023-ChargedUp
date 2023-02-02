c17d5bdfb26172204aa111b7dff402ffa4ba013f
BrandonQ <bq7019@gmail.com>
2023-02-02 15:42:33 -0800

    Added private keyword for private variables

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -18,13 +18,13 @@ import frc.robot.Constants;
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
 
-  CANSparkMax m_armMotor = new CANSparkMax(Constants.k_armMotor, MotorType.kBrushless);
-  CANSparkMax m_armFollower = new CANSparkMax(Constants.k_armFollower, MotorType.kBrushless);
+  private CANSparkMax m_armMotor = new CANSparkMax(Constants.k_armMotor, MotorType.kBrushless);
+  private CANSparkMax m_armFollower = new CANSparkMax(Constants.k_armFollower, MotorType.kBrushless);
 
   // Combine motors
-  MotorControllerGroup m_arm = new MotorControllerGroup(m_armMotor, m_armFollower);
+  private MotorControllerGroup m_arm = new MotorControllerGroup(m_armMotor, m_armFollower);
 
-  RelativeEncoder m_armEncoder = m_armMotor.getEncoder();
+  private RelativeEncoder m_armEncoder = m_armMotor.getEncoder();
 
   private SparkMaxLimitSwitch m_forwardLimit;
   private SparkMaxLimitSwitch m_reverseLimit;
