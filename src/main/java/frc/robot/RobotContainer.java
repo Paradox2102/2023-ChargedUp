d8f9ecc9f6931470f8b9c35f74a5d6304fa00cd6
Isa <108543156+bluskyz11@users.noreply.github.com>
2023-02-07 20:04:29 -0800

    Faster Outtake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,7 +121,7 @@ public class RobotContainer {
     m_stick2.button(8).whileTrue(new ManualReachCommand(m_reachSubsystem, .5));
     m_stick2.button(7).whileTrue(new ManualReachCommand(m_reachSubsystem, -.5));
     m_stick2.button(2).whileTrue(new IntakeCommand(m_intakeSubsystem, -0.8));
-    m_stick2.button(1).whileTrue(new IntakeCommand(m_intakeSubsystem, 0.8));
+    m_stick2.button(1).whileTrue(new IntakeCommand(m_intakeSubsystem, 1));
     m_armSubsystem.setDefaultCommand(new ManualArmCommand(m_armSubsystem, () -> m_stick2.getY()));
   }
 
