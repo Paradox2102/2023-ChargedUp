0c35513afd655c49b939b11b611c554c37b953ac
cpaggron <christopher.periodictable.pi@gmail.com>
2023-03-21 21:17:34 -0700

    Update Auto_5L.java

--- src/main/java/frc/robot/commands/Autos/Auto_5L.java
+++ src/main/java/frc/robot/commands/Autos/Auto_5L.java
@@ -41,7 +41,9 @@ public class Auto_5L extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_timer.stop();
+  }
 
   // Returns true when the command should end.
   @Override
